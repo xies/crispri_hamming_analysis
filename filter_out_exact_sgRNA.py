1b2f61d042a946b2bddd974fc071f1ada7d11474
Shicong Xie <xies@stanford.edu>
2018-10-22 15:11:37 -0700

    Adding code to handle the other strand

--- filter_out_exact_sgRNA.py
+++ filter_out_exact_sgRNA.py
@@ -8,42 +8,77 @@ Created on Fri Oct 19 15:19:45 2018
 
 import numpy as np
 import re
-import pandas as pd
-from Bio import SeqIO, SeqRecord
-from itertools import count, izip
+from Bio import SeqIO, SeqRecord, Seq
+from itertools import product
 
 # Load ChIP peaks
 filename = '/data/crispri_hamming/oct4_chip_flanking.fa'
 seqs = [rec for rec in SeqIO.parse(filename,'fasta')]
 Npeaks = len(seqs)
+has_guide = np.zeros(Npeaks,dtype=bool)
+Lpeak = len(seqs[0]) - 1
 
 # Load output of Ali's GG-finding file
 guide_seqs = []
 repeats = []
-for rec in seqs:
-    # Use positive lookahead to search through all matches
-    pam_sites = [m.start() for m in re.finditer('(?=GG)',str(rec.seq))]
+for (i,rec) in enumerate(seqs):
+    # Use positive lookahead to search through all matches on + strand
+    pam_sites_plus = list(product([m.start() for m in re.finditer('(?=GG)',str(rec.seq))],'+'))
+    # Combine with the reverse complement to search through on - strand
+    revcomp = str(rec.reverse_complement().seq)
+    pam_sites_minus = list(product([m.start() for m in re.finditer('(?=GG)',revcomp)],'-'))
     # Filter through ones that have 20 flanking positions
-    pam_sites = [s for s in pam_sites if s > 19]
+    pam_sites = [s for s in pam_sites_plus if s > 19]
+    pam_sites = pam_sites + [s for s in pam_sites_minus if s > Lpeak - 19]
     for s in pam_sites:
-        peak_locus = get_genomic_locus_begin(rec.name)
-        chr_name = get_chromosome_name(rec.name)
-        new_locus = peak_locus + s - 21
-        newID = ':'.join((chr_name,str(new_locus)))
-        sequence = rec.seq[s-21:s-1]
+        strand = s[1]; s = s[0]
+        chr_name = get_peak_chromosome(rec.name)
+        # Grab sequence depending on strand
+        if strand == '+':
+            peak_locus = get_peak_location(rec.name)[0]
+            new_locus = peak_locus + s - 21
+            sequence = rec.seq[s-21:s-1]
+            newID = ':'.join((chr_name,str(new_locus),'+'))
+        else:
+            peak_locus = get_peak_location(rec.name)[1]
+            new_locus = peak_locus - s + 21
+            newID = ':'.join((chr_name,str(new_locus),'-'))
+            
         if len(sequence) == 0:
             break
         # Check for unique entries
         if newID not in [guide.id for guide in guide_seqs]:
+            if strand == '+':
+                sequence = data=rec.seq[s-21:s-1]
+                newrec = SeqRecord.SeqRecord( seq = sequence, id = newID, description = rec.name )
+            elif strand == '-':
+                sequence = Seq.Seq( data=revcomp[s-21:s-1] )
+                newrec_rc = SeqRecord.SeqRecord( seq = sequence )
+                newrec = newrec_rc.reverse_complement()
+                newrec.id = newID
+                newrec.description = rec.name
             # Generate a SeqRecord
-            guide_seqs.append( SeqRecord.SeqRecord(
-                    seq = sequence,
-                    id = newID,
-                    description = rec.name) )
+            guide_seqs.append( newrec)
+            # note that this peak has a guide
+            has_guide[i] = True
         else:
+            has_guide[i] = True
             repeats.append( SeqRecord.SeqRecord(
                     seq = rec.seq[s-21:s-1],
                     id = newID,
                     description = rec.name) )
 
+
+print float(has_guide.sum()) / Npeaks
+
 SeqIO.write(guide_seqs,'/data/crispri_hamming/sgRNA_unique.fasta','fasta')
+
+def get_peak_location(name):
+    start = name.split(':')[1]
+    end = start.split('-')[1]
+    start = start.split('-')[0]
+    return int(start), int(end)
+
+def get_peak_chromosome(name):
+    return name.split(':')[0]
+
--- find_unique_sgRNA_coverage.py
+++ find_unique_sgRNA_coverage.py
@@ -9,9 +9,7 @@ Created on Sat Oct 20 20:43:45 2018
 import numpy as np
 import matplotlib.pylab as plt
 from Bio import SeqIO, SeqRecord
-from itertools import count, izip
-
-guide_window = 75
+from itertools import count, izip, product
 
 # Load ChIP peaks
 filename = '/data/crispri_hamming/oct4_chip_flanking.fa'
@@ -20,7 +18,35 @@ Npeaks = len(peaks)
 
 # Merge ChIP peaks that are within 75bp
 
+guide_window = 75
 
+df = seqrec2df(peaks)
+chromosomes = np.unique(df['chromosome'])
+unique_peaks = pd.DataFrame()
+for c in chromosomes:
+    this_chrom = df[ df['chromosome'] == c]
+    
+    if len(this_chrom) > 1: # skip if there's only one
+        
+        intervals = zip(this_chrom['start'],this_chrom['end'])
+        # Build the index of pairs
+        II = xrange(len(intervals))
+        index = list(product(II,II))
+        
+        D = np.zeros((len(II),len(II)),dtype=bool)
+        	# Iterate through pairs of intervals using itertools
+        for i, intervPair in izip(count(), product(intervals,intervals)):
+        		ind = index[i]
+        		# Only check one side of diagonal
+        		if ind[0] > ind[1]:
+        			D[ind[0],ind[1]] = overlap(intervPair[0],intervPair[1]) > 20
+	df.loc[df.chromosome == c, 'unique'] = ~np.any(D,axis=0)
+#    for (i,I) in enumerate(intervals):
+#        j = intervals[j]
+#        while 
+            
+        
+    
 # Load sgRNA seqs
 filename = '/data/crispri_hamming/sgRNA_unique.fasta'
 guides = [rec for rec in SeqIO.parse(filename,'fasta')]
@@ -43,12 +69,27 @@ for (i,p) in enumerate(peaks):
         max_distances[i] = D[indices,0].max()
     
 # Plot the inverse distribution of whether a ChIP peak would have a sgRNA "unique" enough
-outs = plt.hist(max_distances[max_distances >= 0],cumulative=True,normed=True)
+bins = range(int(max_distances.max())+2)
+outs = plt.hist(max_distances[max_distances >= 0],bins=bins,
+                cumulative=True,normed=True,align='mid',type='step')
+N = outs[0];
+plt.bar(bins[:-1],1-N);
+plt.xlabel('Minimum mismatches to other sgRNAs requred');
+plt.ylabel('Fraction of ChIP peaks')
 
-def peaks_overlapping(p1,p2,w):
-    start1 = 
-    return
-
+def seqrec2df(peaks):
+    # Parse the nameing convention of the ChIP peaks into a machine-readable dataframe
+    import pandas as pd
+    
+    temp_list = []
+    for rec in peaks:
+        chrom = get_peak_chromosome(rec.name)
+        I = get_peak_location(rec.name)
+        temp_list.append([rec.name,rec.seq,chrom,I[0],I[1],True])
+    
+    return pd.DataFrame(data = temp_list,
+                        columns = ['name','sequence','chromosome','start','end','unique'])
+    
 def get_peak_chromosome(name):
     return name.split(':')[0]
 
@@ -56,7 +97,7 @@ def get_peak_location(name):
     start = name.split(':')[1]
     end = start.split('-')[1]
     start = start.split('-')[0]
-    return start, end
+    return int(start), int(end)
 
 def get_peak_name(desc):
     return desc.split(' ')[1]
